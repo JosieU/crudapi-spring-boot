51855ec9d91e4a0911ce88fe82e27e3b8240b65d
Josie <munezerojosie@gmail.com>
2023-07-20 04:50:35 -0700

    Creating and Deleting Customers API

--- src/main/java/com/jojo/Main.java
+++ src/main/java/com/jojo/Main.java
@@ -2,9 +2,7 @@ package com.jojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,13 +13,39 @@ public class Main {
 
     private final Customer_Repository customerRepository;
     public Main(Customer_Repository customerRepository){
+
         this.customerRepository = customerRepository;
     }
 
     public static void main(String []args){
+
         SpringApplication.run(Main.class,args);
     }
+    public List<Customer> getCustomers(){
+        return  customerRepository.findAll();
+    }
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
 
     }
-    
+    public void addCustomer( @RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+
+
+    }
+    @DeleteMapping("{customerId}")
+    public  void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+
+}
+
 
